f9145f648a216012d53e3da11b431b8a059d962d
Ryan <ryanshaunsmith@gmail.com>
2025-01-25 08:15:32 +0000

    update html structure of the form and minor styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,30 +13,36 @@ function App() {
         <div className="add-data-point">
           <h2>Add info about your migraine</h2>
           <form>
-            <label>
-              Date:
-              <input
-                type="date"
-                name="date"
-                defaultValue={new Date().toISOString().split('T')[0]}
-              ></input>
-            </label>
-            <label>
-              Time:<input type="time" name="time"></input>
-            </label>
-            <label>
-              Intensity
-              <input type="range" name="Intensity" min="1" max="10"></input>
-            </label>
-            <label>
-              Preceding Symptoms<input type="text" name="symptoms"></input>
-            </label>
-            <label>
-              Triggers<input type="text" name="triggers"></input>
-            </label>
-            <label>
-              Medication<input type="text" name="medication"></input>
-            </label>
+            <div className="date-time">
+              <div className="form-input">
+                <label htmlFor="date">Date:</label>
+                <input
+                  type="date"
+                  name="date"
+                  defaultValue={new Date().toISOString().split('T')[0]}
+                ></input>
+              </div>
+              <div className="form-input">
+                <label htmlFor="time">Time:</label>
+                <input type="time" name="time"></input>
+              </div>
+            </div>
+            <div className="form-input">
+              <label htmlFor="intensity">Intensity</label>
+              <input type="range" name="intensity" min="1" max="10"></input>
+            </div>
+            <div className="form-input">
+              <label htmlFor="">Preceding Symptoms</label>
+              <textarea name="symptoms" rows={3}></textarea>
+            </div>
+            <div className="form-input">
+              <label htmlFor="triggers">Triggers</label>
+              <textarea name="triggers" rows={3}></textarea>
+            </div>
+            <div className="form-input">
+              <label htmlFor="medication">Medication</label>
+              <textarea name="medication" rows={3}></textarea>
+            </div>
             <button onClick={() => setCount((count) => count + 1)}>
               count is {count}
             </button>
